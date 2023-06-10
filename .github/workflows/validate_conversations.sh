c124e0c89972dfd509183547970b2cd37b7597ef
Vladimir Orany <vladimir@orany.cz>
2023-06-10 07:31:47 +0200

    real validation command

--- .github/workflows/validate_conversations.sh
+++ .github/workflows/validate_conversations.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Define the validation command
-VALIDATION_COMMAND="docker run -v $(pwd):/data 693201755147.dkr.ecr.eu-west-1.amazonaws.com/markdown-academy-cli:latest validate"
+set -e
+
+CURRENT_DIR="$(pwd)"
 
 # Function to validate a single file
 validate_file() {
     local file="$1"
-    echo "Validating: $file"
-    $VALIDATION_COMMAND "/data/$file"
+    if [ -f "$file" ]; then
+        echo "Validating: $file"
+        docker run -v "$(pwd):/data" 693201755147.dkr.ecr.eu-west-1.amazonaws.com/markdown-academy-cli:latest validate "/data/$file"
+    elif [ -d "$file" ]; then
+        process_files "$file"
+    fi
 }
 
 # Recursive function to process files
 process_files() {
     local dir="$1"
-    for file in "$dir"/*.md; do
-        if [ -f "$file" ]; then
-            validate_file "$file"
-        elif [ -d "$file" ]; then
-            process_files "$file"
-        fi
-    done
+    while IFS= read -r -d '' file; do
+        validate_file "$file"
+    done < <(find "$dir" -type f -name "*.md" -print0)
 }
 
 # Start processing files in the conversations folder
-process_files "conversations"
+process_files "$CURRENT_DIR/conversations"
